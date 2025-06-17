a11c7fa207928a98edb360f9a7d7d024e1387a8d
gaetanopyro <gaetano.miscio@gmail.com>
2025-06-17 15:07:06 +0200

    add

---
+++ src/Exercise_1/Main.java
@@ -0,0 +1,10 @@
+package Exercise_1;
+
+import java.util.Scanner;
+
+public class Main {
+    public static String stringaPariDispari (String word) {
+        return word.length() % 2 == 0
+
+    }
+}
---
+++ src/Exercise_2/Main.java
@@ -0,0 +1,6 @@
+package Exercise_2;
+
+public class Main {
+    public static void main(String[] args)
+    }
+}
