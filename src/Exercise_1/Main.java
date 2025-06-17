e30c1a3dac08ee6b95577b456d7fb8561c551bd0
gaetanopyro <gaetano.miscio@gmail.com>
2025-06-17 16:09:14 +0200

    add exercise 1

--- src/Exercise_1/Main.java
+++ src/Exercise_1/Main.java
@@ -1,10 +1,31 @@
 package Exercise_1;
 
-import java.util.Scanner;
-
 public class Main {
-    public static String stringaPariDispari (String word) {
-        return word.length() % 2 == 0
+
+    public static boolean stringaPariDispari (String word) {
+        return word.length() % 2 == 0;
+
+}
+    public static boolean annoBisestile (int year) {
+        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+    }
+    public static void main(String[] args) {
+        System.out.println("stringa pari e dispari");
+        String stringa_1 = "ciao";
+        String stringa_2 = "come stai";
+        System.out.println(stringaPariDispari(stringa_1));
+        System.out.println(stringaPariDispari(stringa_2));
+        System.out.println("anno bisestile");
+        int year_1 = 2000;
+        int year_2 = 1900;
+        int year_3 = 2024;
+        int year_4 = 2023;
+        System.out.println(annoBisestile(year_1));
+        System.out.println(annoBisestile(year_2));
+        System.out.println(annoBisestile(year_3));
+        System.out.println(annoBisestile(year_4));
+
 
     }
+
 }
--- src/Exercise_2/Main.java
+++ src/Exercise_2/Main.java
@@ -1,6 +1,5 @@
 package Exercise_2;
 
-public class Main {
-    public static void main(String[] args)
-    }
-}
+//public class Main {
+//    public static void main(String[] args)
+
