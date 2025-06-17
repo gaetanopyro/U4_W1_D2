aefb1b7b6301e2baa1626b6f39b6723c602a7b14
gaetanopyro <gaetano.miscio@gmail.com>
2025-06-17 16:37:32 +0200

    add exercise 2

--- src/Exercise_2/Main.java
+++ src/Exercise_2/Main.java
@@ -1,5 +1,31 @@
 package Exercise_2;
 
-//public class Main {
-//    public static void main(String[] args)
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("inserisci un numero");
+        int numb = scanner.nextInt();
+
+        switch(numb) {
+            case 0 :
+                System.out.println(" il numero è zero");
+                break;
+            case 1 :
+                System.out.println(" il numero è uno");
+                break;
+            case 2 :
+                System.out.println(" il numero è due");
+                break;
+            case 3 :
+                System.out.println(" il numero è tre");
+                break;
+            default:
+                System.out.println("errore il numero non è compreso tra 0 e 3");
+        }
+        scanner.close();
+
+    }
+}
 
